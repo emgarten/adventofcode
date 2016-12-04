faf01d54c7ce99ca32a12c3caed50f12ab3a81e1
Justin Emgarten <justin@emgarten.com>
2016-12-03 16:18:20 -0800

    Updating d2p1 to work in the same way as d2p2

--- adventofcode2016/src/adventofcode2016/day02.clj
+++ adventofcode2016/src/adventofcode2016/day02.clj
@@ -2,52 +2,66 @@
   (:use [clojure.pprint])
   (:require [clojure.string :as str]))
 
-(defn to-key-id
-  "x -> Base3 - 1"
-  [x]
-  (Integer. (Integer/toString (- x 1) 3)))
+; Key on a keypad
+(defrecord Key [id row col])
 
-(defn from-key-id
-  "x -> Base10 + 1"
-  [x]
-  (+ 1 (+ (mod x 10) (* 3 (int (Math/floor (/ x 10)))))))
+; Part 1
+(def keypad-1
+  [
+    (->Key "1" 0 0)
+    (->Key "2" 0 1)
+    (->Key "3" 0 2)
+    (->Key "4" 1 0)
+    (->Key "5" 1 1)
+    (->Key "6" 1 2)
+    (->Key "7" 2 0)
+    (->Key "8" 2 1)
+    (->Key "9" 2 2)])
 
-(defn letter-fun
-  "Get letter function"
-  [letter]
-  (case (str letter)
-    "L" #(- % 1)
-    "R" #(+ % 1)
-    "U" #(- % 10)
-    "D" #(+ % 10)
-    #(identity %)))
+(defn match-key-id
+  [key key-id]
+  (= key-id (:id key)))
 
-(defn apply-letter-or-skip
-  [key-id letter]
-  (let [next-id ((letter-fun letter) key-id)]
-    (if
-      (and
-       (> next-id -1)
-       (< next-id 23)
-       (> 3 (mod next-id 10)))
-      ; valid
-      next-id
-      ; noop
-      key-id)))
+(defn get-key-from-keypad
+  ([keypad key-id]
+   (let [result (first (filter #(match-key-id % key-id) keypad))]
+     result))
+  ([keypad row col]
+   (let [result
+         (first
+          (filter
+             #(and (= row (:row %)) (= col (:col %)))
+           keypad))]
+     result)))
+
+(defn get-next-key-id
+  [keypad key-id letter]
+  (let [current-key (get-key-from-keypad keypad key-id)
+        next-key
+          (case (str letter)
+            "L" (get-key-from-keypad keypad (:row current-key) (dec (:col current-key)))
+            "R" (get-key-from-keypad keypad (:row current-key) (inc (:col current-key)))
+            "U" (get-key-from-keypad keypad (dec (:row current-key)) (:col current-key))
+            "D" (get-key-from-keypad keypad (inc (:row current-key)) (:col current-key))
+            (get-key-from-keypad keypad key-id))]
+      (cond
+        (nil? next-key) key-id
+        :else (:id next-key))))
 
 (defn process-single-key
-  [[letter & other-letters] key-id]
+  [keypad [letter & other-letters] key-id]
   (cond
     (nil? letter) key-id
     :else (recur
+            keypad
             other-letters
-            (apply-letter-or-skip key-id letter))))
+            (get-next-key-id keypad key-id letter))))
 
 (defn process-keys
-  [[line & other-lines] key-ids]
+  [keypad [line & other-lines] key-ids]
   (cond
     (nil? line) key-ids
-    :else (recur other-lines (conj key-ids (process-single-key line (last key-ids))))))
+    :else (recur keypad other-lines (conj key-ids (process-single-key keypad line (last key-ids))))))
 
 (defn get-lines
   [input-data]
@@ -58,8 +72,8 @@
 (defn solve-part-1
   "Read and process day2 input to find the keypad combination."
   [input-data]
-  (map #(from-key-id %)
-    (rest
-     (process-keys
-      (get-lines input-data)
-      [11]))))
+  (rest
+   (process-keys
+    keypad-1
+    (get-lines input-data)
+    ["5"])))
--- adventofcode2016/test/adventofcode2016/day02_test.clj
+++ adventofcode2016/test/adventofcode2016/day02_test.clj
@@ -4,37 +4,23 @@
 
 (deftest solve-part-1-test
   (testing "Single line"
-    (is (= '(1) (solve-part-1 "ULL"))))
+    (is (= '("1") (solve-part-1 "ULL"))))
   (testing "Four lines"
-    (is (= '(1 9 8 5) (solve-part-1 "ULL
+    (is (= '("1" "9" "8" "5") (solve-part-1 "ULL
                                RRDDD
                                LURDL
                                UUUUD \n "))))
   (testing "Complex"
-    (is (= '(9 5) (solve-part-1 "DRRRRRRRRRRRRRUUUUUUUDUDUDUDUDULLDDRR\nULLR")))))
+    (is (= '("9" "5") (solve-part-1 "DRRRRRRRRRRRRRUUUUUUUDUDUDUDUDULLDDRR\nULLR")))))
 
-(deftest process-single-key-test
-  (testing "9 -> L -> 8"
-    (is (= 21 (process-single-key "L" 22))))
-  (testing "8 -> U -> 5"
-    (is (= 11 (process-single-key "U" 21))))
-  (testing "5 -> R -> 6"
-    (is (= 12 (process-single-key "R" 11))))
-  (testing "6 -> D -> 9"
-    (is (= 22 (process-single-key "D" 12))))
-  (testing "9 -> L -> 8"
-    (is (= 21 (process-single-key "L" 22)))))
-
-(deftest from-key-id-test
+(deftest get-key-from-keypad-by-id-test
   (testing "1"
-    (is (= 1 (from-key-id 0))))
+    (is (= "1" (:id (get-key-from-keypad keypad-1 "1"))))))
+
+(deftest get-key-from-keypad-by-row-col-test
+  (testing "1"
+     (is (= "1" (:id (get-key-from-keypad keypad-1 0 0)))))
   (testing "2"
-    (is (= 2 (from-key-id 1))))
-  (testing "3"
-    (is (= 3 (from-key-id 2))))
-  (testing "4"
-    (is (= 4 (from-key-id 10))))
-  (testing "5"
-    (is (= 5 (from-key-id 11))))
-  (testing "6"
-    (is (= 6 (from-key-id 12)))))
+    (is (= "2" (:id (get-key-from-keypad keypad-1 0 1)))))
+  (testing "9"
+    (is (= "9" (:id (get-key-from-keypad keypad-1 2 2))))))
