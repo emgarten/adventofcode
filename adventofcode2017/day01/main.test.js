f51d5b71af4778935f0611321ac8883ed1d0b7f1
Justin Emgarten <justin@emgarten.com>
2018-02-04 16:02:05 -0800

    day01 part 2

--- adventofcode2017/day01/main.test.js
+++ adventofcode2017/day01/main.test.js
@@ -1,4 +1,7 @@
-const sumMatch = require('./main');
+//const sumMatch = require('./main');
+const main = require('./main');
+const sumMatch = main.sumMatch;
+const sumMatchHalf = main.sumMatchHalf;
 
 test('1122', () => {
   expect(sumMatch("1122")).toBe(3);
@@ -14,4 +17,24 @@ test('1234', () => {
 
 test('91212129', () => {
   expect(sumMatch("91212129")).toBe(9);
+});
+
+test('1212', () => {
+  expect(sumMatchHalf("1212")).toBe(6);
+});
+
+test('1221', () => {
+  expect(sumMatchHalf("1221")).toBe(0);
+});
+
+test('123425', () => {
+  expect(sumMatchHalf("123425")).toBe(4);
+});
+
+test('123123', () => {
+  expect(sumMatchHalf("123123")).toBe(12);
+});
+
+test('12131415', () => {
+  expect(sumMatchHalf("12131415")).toBe(4);
 });
