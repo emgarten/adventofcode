6fa6daab3fb7dc0c9d79230ead9a62ae927d2350
Justin Emgarten <justin@emgarten.com>
2023-12-04 07:33:53 +0000

    day04 part2

--- adventofcode2023/day04/main.go
+++ adventofcode2023/day04/main.go
@@ -27,14 +27,51 @@ func main() {
 		m[c.Id] = c
 	}
 
+	// Part 1
 	total := 0
 	for _, c := range m {
 		x := scoreCard(c)
 		total += x
-		fmt.Printf("Card %v: %v\n", c.Id, x)
+		// fmt.Printf("Card %v: %v\n", c.Id, x)
 	}
 
-	fmt.Printf("Lines: %v total: %v\n", len(lines), total)
+	// Part 2
+	// Start with one copy of each card
+	orig := make([]int, 0)
+	for _, c := range m {
+		orig = append(orig, c.Id)
+	}
+
+	// Play each card and cards won
+	plays := playCount(m, orig)
+
+	fmt.Printf("Lines: %v total: %v plays: %v\n", len(lines), total, plays)
+}
+
+// Recursively play the cards and track how many cards were played
+func playCount(cards map[int]Card, play []int) int {
+	plays := 0
+
+	for _, id := range play {
+		// id may not be valid, these should be skipped
+		c, ok := cards[id]
+		if ok {
+			// Count this current card
+			plays++
+			// fmt.Printf("Card %v\n", id)
+
+			// Wins add the cards after this current one
+			next := make([]int, 0)
+			for i := 0; i < len(intersect(c.Win, c.Other)); i++ {
+				// First win is 0, so add 1 to get the next card
+				next = append(next, id+i+1)
+			}
+
+			plays += playCount(cards, next)
+		}
+	}
+
+	return plays
 }
 
 // Double the score for each matching number
@@ -93,7 +130,7 @@ func getLines(path string) []string {
 
 func handleError(err error) {
 	if err != nil {
-		fmt.Printf("Error: %x \n", err)
+		fmt.Printf("Error: %v \n", err)
 		os.Exit(1)
 	}
 }
